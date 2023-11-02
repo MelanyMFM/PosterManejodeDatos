e66a3eae2bd6439746c8578bdf1c4da89ce6b189
MelanyMFM <mefrancom@unal.edu.co>
2023-11-02 08:33:17 -0500

    Update analisisDatos.r

--- analisisDatos.r
+++ analisisDatos.r
@@ -10,16 +10,48 @@ summary(datos)
 
 library(ggplot2)
 
-# Grafico circular de distribucion de genero en la asignacion de beneficios
+# Unificar "estrato 2" con "ESTRATO 2"
+datos |> 
+  mutate(ESTRATO = replace(ESTRATO, which(ESTRATO == "Estrato 2"), "ESTRATO 2")) -> datos
+datos |> 
+  mutate(ESTRATO = replace(ESTRATO, which(ESTRATO == "Estrato 1"), "ESTRATO 1")) -> datos
+datos |> 
+  mutate(ESTRATO = replace(ESTRATO, which(ESTRATO == "Estrato 3"), "ESTRATO 3")) -> datos
+
+library(dplyr)
+library(lubridate)
+datos |> 
+  mutate(ano = year(`FECHA DE NACIMIENTO`),
+         mes = month(`FECHA DE NACIMIENTO`),
+         dia = day(`FECHA DE NACIMIENTO`)) -> datos
+
+
+# Grafico circular de distribucion de genero
 ggplot(datos, aes(x = "", fill = GÉNERO)) +
   geom_bar(width = 1) +
   coord_polar(theta = "y") +
   labs(title = "Distribución de género") +
   theme_void()
 
-# Unificar "estrato 2" con "ESTRATO 2"
-datos |> 
-  mutate(ESTRATO = replace(ESTRATO, which(ESTRATO == "Estrato 2"), "ESTRATO 2")) -> datos
+# Distribución estrato
+ggplot(datos, aes(x = "", fill = ESTRATO)) +
+  geom_bar(width = 1) +
+  coord_polar(theta = "y") +
+  labs(title = "Distribución de Estrato") +
+  theme_void()
 
-
+# Estrato de las victimas del conflicto armado
 victimas_data <- datos[datos$'VICTIMA DEL CONFLICTO ARMADO' == 1, ]
+ggplot(victimas_data, aes(x = "", fill = ESTRATO)) +
+  geom_bar(width = 1) +
+  coord_polar(theta = "y") +
+  labs(title = "Estrato de las victimas del conflicto armado") +
+  theme_void()
+ggplot(victimas_data, aes(x = "", fill = `GRUPO ETNICO`)) +
+  geom_bar(width = 1) +
+  coord_polar(theta = "y") +
+  labs(title = "Grupo etnico de las victimas del conflicto armado") +
+  theme_void()
+
+beneficio <- data %>%
+  count(ESTRATO, `BENEFICIO OTORGADO`)
