a8ad5233e69260dade956d05ba1ca0d490e613db
SamuelOrtizT <saortizt@unal.edu.co>
2023-11-13 17:13:39 -0500

    vas bien

--- analisisDatos.r
+++ analisisDatos.r
@@ -1,10 +1,10 @@
 # Lectura de datos
 library(readr)
-datos <- read_csv("GitHub/PosterManejodeDatos/Beneficiaros_de_becas_y_creditos_de_programas_de_acceso_a_la_educaci_n_superior_de_Antioquia_20231031.csv", 
+datos <- read_csv("https://raw.githubusercontent.com/MelanyMFM/PosterManejodeDatos/main/Beneficiaros_de_becas_y_creditos_de_programas_de_acceso_a_la_educaci_n_super.csv", 
                   col_types = cols(`FECHA DE NACIMIENTO` = col_datetime(format = "%m/%d/%Y %H:%M:%S %p")))
 datos |> dim() # Dimensiones base de datos
 
-view(datos)
+datos
 
 summary(datos)
 
